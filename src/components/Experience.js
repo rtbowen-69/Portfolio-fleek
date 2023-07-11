15c30b022f3fcff3bc9bde4af2a2c49a6994ea83
Rod <rodtbowen@gmail.com>
2023-07-11 13:09:48 -0600

    Updating Experiences 07112023

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -1,33 +1,40 @@
 const Experience = () => {
-    return (
-        <div className="experience">
-            <h2>Experience</h2>
+  return (
+    <div className="experience">
+      <h2>Experience</h2>
 
-            <ul>
-                <li>
-                    <i>DeVry University</i> - <strong>Phoenix AZ</strong>, 1983-1986
-                    <ul>
-                        <li>Emphasis on coding and business accounting</li>
-                    </ul>
-                </li>
-                <li>
-                    <i>TechPower Solutions</i> - <strong>Redmond WA</strong>, 2016-2021
-                    <ul>
-                        <li>Warranty Repair Lead</li>
-                        <li>Warehouse manager</li>
-                        <li>Server component installation & testing</li>
-                    </ul>
-                </li>
-                <li>
-                    <i>Certified Environmental Services</i> - <strong>Environmental Professional</strong>, 1998-2016<ul>
-                        <li>Computerized the process of producing Phase I Environmental Site Assesments</li>
-                        <li>Performed Environmental site assessments per the ASTM standards</li>
-                        <li>Expanded the business to include Storm Water Services</li>
-                    </ul>
-                </li>
-            </ul>
-        </div>
-    );
+      <ul>
+        <li>
+          <i>DeVry University</i> - <strong>Phoenix AZ</strong>, 1983-1986
+          <ul>
+            <li>Emphasis on coding and business accounting</li>
+          </ul>
+        </li>
+        <li>
+          <i>TechPower Solutions</i> - <strong>Redmond WA</strong>, 2016-2021
+          <ul>
+            <li>Warranty Repair Lead</li>
+            <li>Warehouse manager</li>
+            <li>Server component installation & testing</li>
+          </ul>
+        </li>
+        <li>
+          <i>Certified Environmental Services</i> - <strong>Environmental Professional</strong>, 1998-2016<ul>
+            <li>Computerized the process of producing Phase I Environmental Site Assesments</li>
+            <li>Performed Environmental site assessments per the ASTM standards</li>
+            <li>Expanded the business to include Storm Water Services</li>
+          </ul>
+        </li>
+        <li>
+          <i>Insight Enterprises</i> - <strong>Tempe AZ</strong>, 1992-1998
+          <ul>
+            <li>Started in direct sales answering customer calls and addressing questions</li>
+            <li>Worked my way up and into sales management and eventually into Training Sales Manager</li>
+          </ul>
+        </li>
+      </ul>
+    </div>
+  );
 }
 
 export default Experience;
